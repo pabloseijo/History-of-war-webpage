f4a2340ebae76d04fbab0f6feab9e5bdae67c0c0
Pablo Seijo <pgarsei510@gmail.com>
2024-03-24 11:53:54 +0100

    JSON: Search bar pages loaded from a json file

--- Proyect/buscador/buscador.js
+++ Proyect/buscador/buscador.js
@@ -1,41 +1,31 @@
-// Supongamos que tienes un arreglo de objetos que representan tus páginas
-let pages = [
-    // Páginas principales
-    {title: "mapa", url: "../Mapa/mapa.html"},
-    {title: "aconteicmientos historicos", url: "../AcontecimientosHistoricos/AH.html"},
-    {title: "biblioteca", url: "../biblioteca/biblioteca.html"},
-    {title: "iniciar sesion", url: "../login/login.html"},
-    {title: "batallas", url: "../AcontecimientosHistoricos/batallas.html"},
-    {title: "guerras", url: "../AcontecimientosHistoricos/guerras.html"},
+let pages = [];
 
-    // Batallas
-    {title: "stalingrado", url: "../AcontecimientosHistoricos/batallas/batallaStalingrado.html"},
-    {title: "stalingrado", url: "../AcontecimientosHistoricos/batallas/batallaStalingrado.html"},
-    {title: "cannas", url: "../AcontecimientosHistoricos/batallas/batallaCannas.html"},
-    {title: "covadonga", url: "../AcontecimientosHistoricos/batallas/batallaCovadonga.html"},
-    {title: "hulao", url: "../AcontecimientosHistoricos/batallas/batallaHulao.html"},
-    {title: "san quintin", url: "../AcontecimientosHistoricos/batallas/batallaSanQuintin.html"},
-    {title: "shangai", url: "../AcontecimientosHistoricos/batallas/batallaShangai.html"},
-    {title: "termopilas", url: "../AcontecimientosHistoricos/batallas/batallaTermopilas.html"},
-    {title: "teutoburgo", url: "../AcontecimientosHistoricos/batallas/batallaTeutoburgo.html"},
+// Función para cargar las páginas desde el archivo JSON usando XMLHttpRequest
+function loadPages() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', './buscador.json', true);
 
-    // Guerras
-    {title: "conquista de america", url: "../AcontecimientosHistoricos/guerras/conquistaAmerica.html"},
-    {title: "an lushan", url: "../AcontecimientosHistoricos/guerras/conquistaAmerica.html"},
-    {title: "conquista de america", url: "../AcontecimientosHistoricos/guerras/guerraAnLushan.html"},
-    {title: "guerra civil rusa", url: "../AcontecimientosHistoricos/guerras/guerraCivilRusa.html"},
-    {title: "guerra civil taiping", url: "../AcontecimientosHistoricos/guerras/guerraCivilTaiping.html"},
-    {title: "guerras napoleonicas", url: "../AcontecimientosHistoricos/guerras/guerrasNapoleonicas.html"},
-    {title: "guerra de los treinta años", url: "../AcontecimientosHistoricos/guerras/guerraTreintaAnhos.html"},
-    {title: "segunda guerra mundial", url: "../AcontecimientosHistoricos/guerras/segundaGuerraMundial.html"},
-    {title: "segunda guerra sino japonesa", url: "../AcontecimientosHistoricos/guerras/segundaGuerraMundiaSino-Japonesa.html"},
+    xhr.onload = function() {
+        if (this.status >= 200 && this.status < 300) {
+            // Parsea y carga los datos en la variable 'pages'
+            pages = JSON.parse(this.responseText);
+        } else {
+            // Maneja errores de respuesta del servidor
+            console.error('Error al cargar el archivo JSON:', this.statusText);
+        }
+    };
 
-    //Libros
-    {title: "libros edad antigua", url: "../biblioteca/LibrosEA"},
-    {title: "libros edad media", url: "../biblioteca/LibrosEMed"},
-    {title: "libros edad moderna", url: "../biblioteca/LibrosEM"},
-    {title: "libros edad contemporanea", url: "../biblioteca/LibrosEC"},
-];
+    xhr.onerror = function() {
+        // Maneja errores de red
+        console.error('Error de red.');
+    };
+
+    xhr.send();
+}
+
+
+// Cargar las páginas al iniciar
+loadPages();
 
 document.getElementById('searchForm').onsubmit = function(event) {
     document.getElementById('noResults').style.display = 'none';
